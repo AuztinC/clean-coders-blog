3a0d31a8dad82daa759a24742c1946552335c555
AuztinC <40247162+AuztinC@users.noreply.github.com>
2025-04-01 19:44:59 -0400

    end of day update

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -1,7 +1,7 @@
 const blogPosts = [
   {title: "Day One",
-    blog: "This is my first blog post. Today I learned about a lot of tools that I will use to be successful at Clean Coders. Tuple, CC-Epic, and CC-Poker. I used Tuple to partner with my mentor Alex where we discussed the process of estimating jobs and features. I will proceed to setup the environment on the Mac Mini that he has provided me as well as watch the first video if time permits!",
-    date: "4/1/2025 1:50pm",
+    blog: "This is my first blog post. Today I learned about a lot of tools that I will use to be successful at Clean Coders. Tuple, CC-Epic, and CC-Poker. I used Tuple to partner with my mentor Alex where we discussed the process of estimating jobs and features. After deploying this blog site, I will proceed to setup the environment on the Mac Mini that he has provided me as well as watch the first video if time permits! \n End of the day update. The Mac Mini is set up with the proper environment for the clojure koans and the first few koans are complete.",
+    date: "4/1/2025",
   },
 
   
