c199a416a158f28aae885aeb74cb9dbdf0286504
AuztinC <40247162+AuztinC@users.noreply.github.com>
2025-04-01 14:00:39 -0400

    ready to roll

---
+++ src/BlogPosts.tsx
@@ -0,0 +1,10 @@
+const blogPosts = [
+  {title: "Day One",
+    blog: "This is my first blog post. Today I learned about a lot of tools that I will use to be successful at Clean Coders. Tuple, CC-Epic, and CC-Poker. I used Tuple to partner with my mentor Alex where we discussed the process of estimating jobs and features. I will proceed to setup the environment on the Mac Mini that he has provided me as well as watch the first video if time permits!",
+    date: "4/1/2025 1:50pm",
+  },
+
+  
+]
+
+export default blogPosts
