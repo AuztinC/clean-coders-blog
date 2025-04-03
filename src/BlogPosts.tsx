60cefda32d005b79ef27c170cd10c7e800f92a97
AuztinC <40247162+AuztinC@users.noreply.github.com>
2025-04-03 18:31:30 -0400

    day 3

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -1,11 +1,15 @@
 const blogPosts = [
-  {title: "Day One",
+  {title: "Day One - Blast Off!",
     blog: "This is my first blog post. Today I learned about a lot of tools that I will use to be successful at Clean Coders. Tuple, CC-Epic, and CC-Poker. I used Tuple to partner with my mentor Alex where we discussed the process of estimating jobs and features. After deploying this blog site, I will proceed to setup the environment on the Mac Mini that he has provided me as well as watch the first video if time permits! The Mac Mini is set up with the proper environment for the clojure koans and the first few koans are complete.",
     date: "4/1/2025",
   },
-  {title: "Day Two",
+  {title: "Day Two - Diving in",
     blog: "Koans! Today I did Koans 7 - 20. I got stuck on a couple of the problems but proceeded to use the lein REPL to evaluate expressions and figure out my answers. They have taken me longer to complete than I estimated!",
     date: "4/2/2025",
+  },
+  {title: "Day Three - Taking a closer look",
+    blog: "There is no shortage of information to be learned! After pushing through a couple of more Koans, I met with Alex J and understand some of the Koans I had earlier got to pass but didn't fully understand why. I now understand meta data a bit better! I found myself going into docs today to read more about some Clojure functions (seq vs sequence, meta, vary-meta). Alex Root popped in to say welcome, which was very appreciated and made me feel like a part of the team even more!",
+    date: "4/3/2025",
   },
 
   
