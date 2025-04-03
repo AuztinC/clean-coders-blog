2e35f1c47561d4d6eeb7a86c5119bd931e462b13
AuztinC <40247162+AuztinC@users.noreply.github.com>
2025-04-03 12:04:52 -0400

    Update BlogPosts.tsx

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -3,6 +3,10 @@ const blogPosts = [
     blog: "This is my first blog post. Today I learned about a lot of tools that I will use to be successful at Clean Coders. Tuple, CC-Epic, and CC-Poker. I used Tuple to partner with my mentor Alex where we discussed the process of estimating jobs and features. After deploying this blog site, I will proceed to setup the environment on the Mac Mini that he has provided me as well as watch the first video if time permits! The Mac Mini is set up with the proper environment for the clojure koans and the first few koans are complete.",
     date: "4/1/2025",
   },
+  {title: "Day Two",
+    blog: "Koans! Today I did Koans 7 - 20. I got stuck on a couple of the problems but proceeded to use the lein REPL to evaluate expressions and figure out my answers. They have taken me longer to complete than I estimated!",
+    date: "4/2/2025",
+  },
 
   
 ]
