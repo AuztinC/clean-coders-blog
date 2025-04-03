fae7668f28506960d5e2740e56412ff74a86bcb0
AuztinC <40247162+AuztinC@users.noreply.github.com>
2025-04-03 18:33:15 -0400

    Update BlogPosts.tsx

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -4,11 +4,11 @@ const blogPosts = [
     date: "4/1/2025",
   },
   {title: "Day Two - Diving in",
-    blog: "Koans! Today I did Koans 7 - 20. I got stuck on a couple of the problems but proceeded to use the lein REPL to evaluate expressions and figure out my answers. They have taken me longer to complete than I estimated!",
+    blog: "Koans! Today I did Koans 7 - 20. Going back through the Koans a second time has been a great refresher. I got stuck on a couple of the problems but proceeded to use the lein REPL to evaluate expressions and figure out my answers. Taking deeper dives into some of the key functions I learned the power of 'Let' to bind data structures to symbols. This is great way to destructure a datatype. 'Ref' will help us create a mutable object which I imagine could be great for managing things like state. ",
     date: "4/2/2025",
   },
   {title: "Day Three - Taking a closer look",
-    blog: "There is no shortage of information to be learned! After pushing through a couple of more Koans, I met with Alex J and understand some of the Koans I had earlier got to pass but didn't fully understand why. I now understand meta data a bit better! I found myself going into docs today to read more about some Clojure functions (seq vs sequence, meta, vary-meta). Alex Root popped in to say welcome, which was very appreciated and made me feel like a part of the team even more!",
+    blog: "There is no shortage of information to be learned! After pushing through a couple of more Koans, I met with Alex J and understand some of the Koans I had earlier got to pass but didn't fully understand why. I now understand meta data a bit better, The ability to attach underlying data to an object. I found myself going into docs today to read more about some Clojure functions (seq vs sequence, meta, vary-meta, and more).\n Alex Root popped in to say welcome, which was very appreciated and made me feel like a part of the team even more! Macros are confusing. After spending some time trying to understand where I was going wrong I again asked for some help and got clarification on the 'recursive-infix' problem within macros. After restructuring the macro being create I was lead to find a further use for the 'drop' function as well as nthrest(the ability to take the end of a form starting at a certain index). \n Koans are complete although I will be scouring back through them in due time. Multimethods are very useful, similar to 'method overloading' in c#; This gives us the ability to have different outputs from the same function based on the inputing parameters.",
     date: "4/3/2025",
   },
 
