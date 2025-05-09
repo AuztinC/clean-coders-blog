650d29f94425b5017d2b1cb7d5e3054355cd8dd2
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-05-09 17:40:48 -0400

    5-9-2025

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -128,6 +128,11 @@ const blogPosts = [
     blog: "\t The twelve transformation that are small changes within your code that will alter the behavior in a specific way. The priority of these behaviors matters and should be used with the highest of available priority transformations to be used in a situation. Watching Uncle Bob solve the example problem and ending up with two different sorting algorithms as a result was a shocking thing to see. Just by adding a computation to a new variable rather than assigning to an existing state, the speed of the algorithm improved by miles! \n\n\t Getting to get a better look at how to properly stub a functions behavior today was great. The ability to check the invokation of a function and spy to see the about of calls. This is very exciting to see the tools used in the language I am actively trying to learn. \n\n\t Ready to present the Interface Segregation Principle tomorrow!",
     date: "5-8-2025"
   },
+      {
+    title: "First week as Curator",
+    blog: "\t My skateboarding theme for the week was a slight success. It was fun to choose people to speak about every morning and share some of my passion for skateboarding with my team. I believe Jason Lee was the winner of the week. Being a famous actor helped create add something for people to relate to, if they have seen the show even once. \n\n\t Presenting the Interface Segregation principle went well this week. Showing the relationship between the ISP and the LSP was a great choice, I only wish I would have thought to mention the tie in with the SRP as well. Alex was right to point it out as they go hand-in-hand. \n\n\t Starting off with the Quil example project was a great introduction to the tool. I am really liking the simplicity of the workflow. Having preset setup, update,and draw abilities built in is extremely helpful for getting started quickly. After fiddling for a while, I am able to create a screen of squares and change random squares color by changing a :on value to 0 or 1. This will come in handy when tying in the logic for the Game of Life. I'm excited to work more with the tool and better understand the Kata itself. A visual project will be a nice to the terminal projects I've been doing.",
+    date: "5-9-2025"
+  },
       // {
   //   title: "",
   //   blog: "\t ",
