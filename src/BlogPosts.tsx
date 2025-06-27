da1296aae8863f3591a8f7657846b10fc20cc115
Austin Cripe <austincripe@Austins-Mini.home.local>
2025-06-27 16:13:31 -0400

    6-27-25

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -270,6 +270,14 @@ const blogPosts = [
     blog: "\t I fell short this week. I didn't have all of my features implemented in my GUI and also didn't fully complete the postgres database work either. This was a hard meeting with Micah and Alex. Failing is a part of the process, though. It is expected and I am surprised I haven't had this big of a failure so far. At least completing one of the two big stories for the week would have been the ideal move. Going forward with my GUI work when my database was not fully completed was a mistake and a lesson learned. \n\n\t That being said, I was still happy with the amount of work that I managed to complete. The task of creating the full gui and tying all of my previous logic into it was a rather large one to manage. I am happy with the tests that I was writing against my business logic for the gui, which I didn't even bring up in the meeting. The focus was definitely on my short-comings. \n\n\t After the tough talk was wrapped up, both of my previous large stories were moved into the next iteration. This only left me with a presentation story in addition to them. I have decided on the Decorator Design pattern due to the similarity to how clojure is extended. \n\n\t Getting to pair with Alex today and work on my postgres implementation was a huge help. Having an idea of what the next test should be is a surprisingly hard thing for me some of the time. Going through and re-writing the entire postgres schema is a huge deal, I am grateful for the help. \n\n\t Leaving today feeling better than it started. I am extremely grateful to be here and have to take full advantage of the opportunity. I won't let my failure last week bring me down.",
     date: "6-25-25"
   },
+  {title: "Decorating the future",
+    blog: "\t The Decorator Design Pattern is a great pattern to keep in mind when working in a functional programming space. This pattern gives us developers a way of building behavior on top of a previous class or interface without going into the code of that object and changing anything. The idea is based on stacking the new functionality together. Creating 'wrappers' that will enhance the previous version of whatever object you had started with. This comes with some caveats that could end up introducing new barriers in the future of your program. If this system isn't designed properly it could be a challenge to remove the layers of a wrapped object. \n\n\t The design pattern does help us follow some SOLID principles like the Single Responsibility and Open/Closed principles. Allowing each new wrapper to focus on a specific behavior ensures SRP is followed and by continuing to build on the original object we are extending its behavior without ever touching the source code. ",
+    date: "6-26-25"
+  },
+  // {title: "",
+  //   blog: "\t ",
+  //   date: "-25"
+  // },
   // {title: "",
   //   blog: "\t ",
   //   date: "-25"
