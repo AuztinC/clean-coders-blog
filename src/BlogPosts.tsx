ae1be4a59ee13dc2fa76fa955d91a5cb59fd1b46
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-07-08 21:41:14 -0400

    7-8-25

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -298,6 +298,11 @@ const blogPosts = [
     blog: "\t Sometimes the things we think we don't need, we actually might. Previously when refactoring the database implementations and thinking that I could solve the problem without keeping track of the actual current game in some fashion, I made a decision to delete my 'clear-active' sort of function. Eventually after deciding to add back an active game using the state map, I found myself needing a function that performed the same essential logic but in a different data set. \n\n\t Getting this setup helped me solidify what my databse solutions needed. Thankfully, only a couple of tests needed to be refactored in order to force this change, and they all seemed to come together nicely. The functionality is back!",
     date: "7-7-25"
   },
+  
+  {title: "Feeling comfortable again",
+    blog: "\t A long awaited feeling when looking at my code! I am starting to see the patterns better and smells are starting to stick out. Today I finished refactoring my CLI game logic by using the state to switch screens. This was a great update that lead me to updating the way my games resume and even initialize. Removing some extra fluff functions in my game logic. \n\n\t The biggest update to Quill was finally put in motion. A refactor of the game-loop itself. I knew when writing the code that I should be utilizing the already made 'next-state' function in my game's core logic. This was tied to a read-line user input for the human's turn however which lead me away from it, not understanding how to get around it. The help from Alex today really helped me solidify my confidence in the fact that it was done initially wrong and that I could and should have taken steps to at least use the next-state for an ai players turn, since their turn is exactly the same in CLI or GUI. Now, my gui has better testing and is more versatile! After, I was back and adding more in depth tests after the refactor to confirm that things are still working if something changes.",
+    date: "7-8-25"
+  },
   // {title: "",
   //   blog: "\t ",
   //   date: "-25"
