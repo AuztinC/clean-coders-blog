525eeba4204209b7e05b8ea9751e457b612e33bf
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-07-17 21:26:38 -0400

    7-17-25

--- src/BlogPosts.tsx
+++ src/BlogPosts.tsx
@@ -323,6 +323,15 @@ const blogPosts = [
     blog: "\t Today marked a big shift—from parsing requests to actually serving files. It finally feels like my HTTP server is becoming a real server. Using java.nio, I figured out how to send files back in the response body, along with the correct Content-Type and other headers like Content-Length. Getting those headers right was satisfying—it made the communication feel complete, not just some hacked-together protocol parsing. \n\t Another big win was tying the test suite to my compiled .jar file. Now I can build the project and run the specs cleanly against the latest version of the server. It’s a small thing, but being able to hit build and see some specs pass gives me real confidence that I’m heading in the right direction. \n\t I still feel the weight of trying to adhere to the principles behind clean code and good architecture, especially in a language that isn’t second nature to me yet. But today was a good day. My server responded, and so did my motivation.",
     date: "7-15-25"
   },
+  {title: "",
+    blog: "\t ",
+    date: "7-16-25"
+  },
+  {title: "",
+    blog: "\t ",
+    date: "7-17-25"
+  },
+  // },
   // {title: "",
   //   blog: "\t ",
   //   date: "-25"
